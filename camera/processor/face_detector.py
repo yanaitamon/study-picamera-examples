a391b666b5a9fcdd83da7b1676e6c0eeb2fd1265
ty <rspm068052@gmail.com>
2018-08-07 20:13:17 +0900

    test for putText.

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -44,7 +44,7 @@ class FaceDetector(object):
         # 顔の位置を描画する
         for (x,y,w,h) in faces:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(128,128,0),2)
-
-        # cv2.putText(frame,'33',(10,100), font, 4,(128,0,255),3,cv2.LINE_AA)
+        
+        cv2.putText(frame,'33',(10,100), font, 4,(128,0,255),3,cv2.LINE_AA)
         # frameを戻り値として返す
         return frame
