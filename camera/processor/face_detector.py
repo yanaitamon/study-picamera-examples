1cf479cf4578e7808647947cb8202bf36bfc74a8
ty <rspm068052@gmail.com>
2018-08-07 20:12:05 +0900

    test for putText.

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -37,7 +37,7 @@ class FaceDetector(object):
         faces = self.face_cascade.detectMultiScale(gray, 1.3, 3)
 
         # 文字列を作成する
-        iNum = faces.length
+        # iNum = faces.shape
         font = cv2.FONT_HERSHEY_SIMPLEX
 
         # 取得した座標情報を元に、cv2.rectangleを使ってframe上に
@@ -45,6 +45,6 @@ class FaceDetector(object):
         for (x,y,w,h) in faces:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(128,128,0),2)
 
-        cv2.putText(frame,'33',(10,100), font, 4,(128,0,255),3,cv2.LINE_AA)
+        # cv2.putText(frame,'33',(10,100), font, 4,(128,0,255),3,cv2.LINE_AA)
         # frameを戻り値として返す
         return frame
